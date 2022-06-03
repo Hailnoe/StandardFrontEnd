503256e00d06d85a75ce0ba75696530f0fac4363
Hailnoe <trnoe@davidson.edu>
2022-06-03 09:33:06 -0400

    Trey  - last - update - 6/3/2022

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var hamburger = Front.querySelector(".wrapper .section .top_navbar .hamburger");
+var hamburger = Front.querySelector(".hamburger");
 
 hamburger.addEventListener("click", function(){
     document.querySelector("body").classList.toggle("active");
