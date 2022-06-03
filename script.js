79f6f49bd43aca61ec18a5a6970b87729313da6d
Hailnoe <trnoe@davidson.edu>
2022-06-03 07:15:19 -0400

    changed js script position

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var hamburger = document.querySelector(".wrapper .section .top_navbar .hamburger");
+var hamburger = Front.querySelector(".wrapper .section .top_navbar .hamburger");
 
 hamburger.addEventListener("click", function(){
     document.querySelector("body").classList.toggle("active");
